a732374a2c64b3ba011cd898544444cc3ffbda88
TenshiAngelina <angelina.hcortes@gmail.com>
2023-02-21 21:37:23 -0600

    Add npm packages

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // TODO: Include packages needed for this application
+const fs = require("fs"); //call for fs
+const inquirer = require("inquirer"); //call for inquirer
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  ``
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt 
+}
 
 // Function call to initialize app
 init();
