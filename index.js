1950e3c65990aa4b433b5bc2d2333b7710e80a6a
TenshiAngelina <angelina.hcortes@gmail.com>
2023-03-20 16:59:12 -0600

    Add initial questions for inquirer, create write README function, create initialize app function

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ const inquirer = require("inquirer"); //call for inquirer
 const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the description of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please, provide instructions and examples of use",
+    name: "usage",
+  }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  ``
+function writeToFile(data) {
+  fs.writeFile('README.md', data, function (err) {
+    if (err) throw err;
+  });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt 
+  inquirer.prompt(questions).then(function(answers) {
+    const templateReadme = generateMarkdown(answers);
+    writeToFile(templateReadme);
+  })
 }
 
 // Function call to initialize app
